19dfec5a86ef4a445be868b7632ffa17aa474d7a
Diana Ma <diana@rarestype.com>
2026-03-24 17:24:40 +0000

    format

--- Sources/TopologicalSortingTests/Sorting.swift
+++ Sources/TopologicalSortingTests/Sorting.swift
@@ -2,65 +2,56 @@ import Testing
 import TopologicalSorting
 
 @Suite enum Sorting {
-    @Test
-    static func Empty() {
+    @Test static func Empty() {
         Self.test(nodes: [], edges: [:])
     }
-    @Test
-    static func Single() {
+    @Test static func Single() {
         Self.test(nodes: ["A"], edges: [:])
     }
-    @Test
-    static func Multiple() {
+    @Test static func Multiple() {
         Self.test(nodes: ["A", "B", "C"], edges: [:])
     }
-    @Test
-    static func LoopbackEdge() {
+    @Test static func LoopbackEdge() {
         Self.test(nodes: ["A", "B", "C"], edges: ["A": "A"], cyclic: true)
     }
 
-    @Test
-    static func Circular() {
+    @Test static func Circular() {
         Self.test(nodes: ["A", "B", "C"], edges: ["A": "B", "B": "C", "C": "A"], cyclic: true)
     }
 
-    @Test
-    static func IgnoredEdge() {
+    @Test static func IgnoredEdge() {
         Self.test(nodes: ["A", "B", "C"], edges: ["A": "D"])
     }
-    @Test
-    static func SingleEdge() {
+    @Test static func SingleEdge() {
         Self.test(nodes: ["B", "A", "C"], edges: ["B": "A"])
     }
-    @Test
-    static func DuplicateEdge() {
+    @Test static func DuplicateEdge() {
         Self.test(nodes: ["B", "A", "C"], edges: ["B": "A", "B": "A"])
     }
 
-    @Test
-    static func ConvergingStar() {
+    @Test static func ConvergingStar() {
         Self.test(
             nodes: ["B", "C", "D", "A"],
             edges: [
                 "B": "A",
                 "C": "A",
                 "D": "A",
-            ])
+            ]
+        )
     }
 
-    @Test
-    static func DivergingStar() {
+    @Test static func DivergingStar() {
         Self.test(
             nodes: ["A", "B", "C", "D"],
             edges: [
                 "A": "B",
                 "A": "C",
                 "A": "D",
-            ])
+            ]
+        )
     }
 
-    @Test
-    static func Lips() {
+    @Test static func Lips() {
         Self.test(
             nodes: ["4", "0", "1", "5", "2", "3"],
             edges: [
@@ -69,11 +60,11 @@ import TopologicalSorting
                 "1": "3",
                 "5": "2",
                 "2": "3",
-            ])
+            ]
+        )
     }
 
-    @Test
-    static func LipsAsymmetric() {
+    @Test static func LipsAsymmetric() {
         Self.test(
             nodes: ["4", "0", "5", "2", "3", "1"],
             edges: [
@@ -82,7 +73,8 @@ import TopologicalSorting
                 "3": "1",
                 "5": "2",
                 "2": "3",
-            ])
+            ]
+        )
     }
 }
 extension Sorting {
